41d7479e4cbf7731c7a447291852a1d9f766bfad
Gabriel Petrovick <petrovickg@hotmail.com>
2019-05-15 11:00:40 -0300

    Correção cors

--- src/routes.js
+++ src/routes.js
@@ -1,50 +1,50 @@
-const express = require('express')
-const validate = require('express-validation')
-const handle = require('express-async-handler')
-const cors = require('cors')
+const express = require("express");
+const validate = require("express-validation");
+const handle = require("express-async-handler");
+const cors = require("cors");
 
-const routes = express.Router()
+const routes = express.Router();
 
-const authMiddleware = require('./app/middlewares/auth')
+const authMiddleware = require("./app/middlewares/auth");
 
-const controllers = require('./app/controllers')
-const validators = require('./app/validators')
+const controllers = require("./app/controllers");
+const validators = require("./app/validators");
 
-//express.use(cors)
+//express.use(cors);
 
 // TEST
-routes.get('/', controllers.TestController.index)
+routes.get("/", controllers.TestController.index);
 
 /**
  * Users & Sessions
  */
 routes.post(
-  '/user/create',
+  "/user/create",
   // validate(validators.User),
   handle(controllers.UserController.save)
-)
+);
 routes.post(
-  '/session/login',
+  "/session/login",
   // validate(validators.User),
   handle(controllers.SessionController.login)
-)
+);
 
 /**
  * From now on Authentication is required
  */
-routes.use(authMiddleware)
+routes.use(authMiddleware);
 
 /**
  * List of Gamers
  */
-routes.post('/game/add', handle(controllers.GameController.addGame))
-routes.get('/game/list', handle(controllers.GameController.list))
+routes.post("/game/add", handle(controllers.GameController.addGame));
+routes.get("/game/list", handle(controllers.GameController.list));
 
 /**
  * Gamer
  */
-routes.post('/gamer/addpoint', handle(controllers.GamerController.addPoints))
-routes.get('/gamer/listall', handle(controllers.GamerController.listAll))
-routes.get('/gamer/getgamer', handle(controllers.GamerController.getGamer))
+routes.post("/gamer/addpoint", handle(controllers.GamerController.addPoints));
+routes.get("/gamer/listall", handle(controllers.GamerController.listAll));
+routes.get("/gamer/getgamer", handle(controllers.GamerController.getGamer));
 
-module.exports = routes
+module.exports = routes;
